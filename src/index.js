36c1a89551268ca7b80118c0227eac7d5b1ef977
Carlos Urquiza <webquiza@users.noreply.github.com>
2021-08-15 14:43:40 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Triangle from './js/example.js';
+
+$(document).ready(function() {
+  $('#triangle-checker-form').submit(function(event) {
+    event.preventDefault();
+    const length1 = parseInt($('#length1').val());
+    const length2 = parseInt($('#length2').val());
+    const length3 = parseInt($('#length3').val());
+    const triangle = new Triangle(length1, length2, length3);
+    const response = triangle.checkType();
+    $('#response').append(`<p>${response}</p>`);
+  });
+});
