cc057191355dd856378ff9e948e412ebeb9c256c
Carlos Urquiza <webquiza@gmail.com>
2021-08-22 14:39:42 -0700

    add basic api functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
-//import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+$(document).ready(function() {
+  $('#compare').click(function() {
+    const lang = $('#language').val();
+    $('#language').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `https://min-api.cryptocompare.com/data/v2/news/?lang=${lang}&api_key=${process.env.API_KEY}`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      $('.showNews').text(`${response.Message}. Check your console!`);
+      console.log(response);
+      /* or use
+      console.log(response.Data[0].url);
+      */
+    }
+  });
+});
+
 
