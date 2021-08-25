68ee6c57c41e1274aac51d6db88f95004fcec7cb
Carlos Urquiza <webquiza@gmail.com>
2021-08-24 19:36:15 -0700

    deploy gh-pages and update README.md
    Co-authored-by: DeAunt'e Hall <godsofolympus88@gmail.com>
    Co-authored-by: Mary Marks <maryleemarks@gmail.com>
    Co-authored-by: Alejandra Goldberg <alexdominguezwebdev@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: "/epikrypto/",
   },
   devtool: 'eval-source-map',  
   devServer: {                 
